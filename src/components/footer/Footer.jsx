48698dafa2ac309e589442f56262d0f0af8b1d5d
rajabovdavlat <rajabovoff1@gmail.com>
2025-09-12 23:22:00 +0500

    adaptiveHeader

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,19 +1,50 @@
-import './Footer.css'; // <- обязательно подключаем
+import "./Footer.css"; // <- обязательно подключаем
 
 const Footer = () => {
   return (
     <footer className='footer'>
       <div className='container'>
-        <div className="footer__wrapp">
-          <h2 className='footer__title'>Ready to cook smarter?</h2>
-          <p className='footer__desc'>
-            Hit the button, pick a recipe, and get dinner on the table—fast.
-          </p>
-          <button className='btn'>Browse recipes</button>
+        <div className='footer__wrapp'>
+          {/* Левая иконка */}
+          <img
+            src='/images/pattern-fork.svg'
+            alt='Left decoration'
+            className='footer__icon footer__icon--left'
+          />
+
+          <div className='footer__content'>
+            <h2 className='footer__title'>Ready to cook smarter?</h2>
+            <p className='footer__desc'>
+              Hit the button, pick a recipe, and get dinner on the table—fast.
+            </p>
+            <button className='btn'>Browse recipes</button>
+          </div>
+
+          {/* Правая иконка */}
+          <img
+            src='/images/pattern-knife.svg'
+            alt='Right decoration'
+            className='footer__icon footer__icon--right'
+          />
+        </div>
+
+        <div className='footer__nav'>
+          <p className='footer__made'>Made with ❤️ and 🥑</p>
+          <div className='footer__social'>
+            <a href='#'>
+              <img src='/images/icon-instagram.svg' alt='Instagram' />
+            </a>
+            <a href='#'>
+              <img src='/images/icon-bluesky.svg' alt='Babushka' />
+            </a>
+            <a href='#'>
+              <img src='/images/icon-tiktok.svg' alt='TikTok' />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
   );
 };
 
-export default Footer; 
+export default Footer;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,22 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../header/Header.css";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <header className='header'>
       <div className='container'>
-        <div className="header-nav__wrapp">
-          {/* Лого */}
+        <div className='header-nav__wrapp'>
+          {/* logo */}
           <Link to='/' className='header__logo'>
             <img src='/images/logo.svg' alt='HealthyFood Logo' />
           </Link>
 
-          {/* Навигация по центру */}
+          {/* Navigation for large screens */}
           <nav className='header__nav'>
             <Link to='/' className='header__link'>
               Home
@@ -24,10 +29,47 @@ const Header = () => {
             </Link>
           </nav>
 
-          {/* Кнопка справа */}
+          {/* button on the right */}
           <div className='header__actions'>
             <button className='btn'>Browse recipes</button>
           </div>
+
+          {/* Burger mobile*/}
+          <button className='header__burger' onClick={toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+
+        {/* Modalka mobile */}
+        <div
+          className={`header__modal ${isOpen ? "header__modal--active" : ""}`}
+        >
+          <nav className='header__modal-nav'>
+            <Link to='/' className='header__modal-link' onClick={toggleMenu}>
+              Home
+            </Link>
+            <Link
+              to='/about'
+              className='header__modal-link'
+              onClick={toggleMenu}
+            >
+              About
+            </Link>
+            <Link
+              to='/recipes'
+              className='header__modal-link'
+              onClick={toggleMenu}
+            >
+              Recipes
+            </Link>
+
+            {/* Кнопка Browse recipes */}
+            <button className='header__modal-btn' onClick={toggleMenu}>
+              Browse recipes
+            </button>
+          </nav>
         </div>
       </div>
     </header>
